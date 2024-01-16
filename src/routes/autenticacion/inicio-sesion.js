3edc78934d38fa405e5cd9f7c0db5668d8aff697
CarlosRuiz2023 <juancarlosruizgomez2000@gmail.com>
2024-01-16 10:56:01 -0600

    recuperacion de contrasenia, bloqueos y activaciones de usuarios agregados

--- src/routes/autenticacion/inicio-sesion.js
+++ src/routes/autenticacion/inicio-sesion.js
@@ -1,22 +1,81 @@
 // IMPORTACIÓN DE LAS BIBLIOTECAS NECESARIAS
-const { Router } = require('express');
-const { check } = require('express-validator');
+const { Router } = require("express");
+const { check } = require("express-validator");
 
 // IMPORTACIONES DE MIDDLEWARES Y DE CONTROLADORES
-const { validarCampos } = require('../../middlewares/validar-campos');
-const { inicioSesion } = require('../../controllers/autentificacion/inicio-sesion-controller');
+const { validarCampos } = require("../../middlewares/validar-campos");
+const {
+  inicioSesion,
+  recuperarContrasenia,
+  cambiarContrasenia,
+  bloquearUsuario,
+  usuarioActivar,
+} = require("../../controllers/autentificacion/inicio-sesion-controller");
 
 // CREACIÓN DEL ENRUTADOR
 const router = Router();
 
 // DEFINICIÓN DE RUTA PARA INICIAR SESIÓN
-router.post('/inicioSesion', [
+router.post(
+  "/inicioSesion",
+  [
     // VALIDACIONES PARA LOS DATOS DE INICIO DE SESIÓN
-    check('correo', 'El correo es obligatorio').isEmail(),
-    check('contrasenia', 'La contraseña es obligatoria').not().isEmpty(),
+    check("correo", "El correo es obligatorio").isEmail(),
+    check("contrasenia", "La contraseña es obligatoria").not().isEmpty(),
     // MIDDLEWARE PARA VALIDAR CAMPOS
-    validarCampos
-], inicioSesion);
+    validarCampos,
+  ],
+  inicioSesion
+);
+
+// DEFINICIÓN DE RUTA PARA RECUPERAR CONTRASEÑA
+router.post(
+  "/recuperarContrasenia",
+  [
+    // VALIDACIONES PARA LOS DATOS DE INICIO DE SESIÓN
+    check("correo", "El correo es obligatorio").isEmail(),
+    // MIDDLEWARE PARA VALIDAR CAMPOS
+    validarCampos,
+  ],
+  recuperarContrasenia
+);
+
+// DEFINICIÓN DE RUTA PARA BLOQUEAR AL USUARIO
+router.post(
+  "/bloquearUsuario",
+  [
+    // VALIDACIONES PARA LOS DATOS DE INICIO DE SESIÓN
+    check("correo", "El correo es obligatorio").isEmail(),
+    // MIDDLEWARE PARA VALIDAR CAMPOS
+    validarCampos,
+  ],
+  bloquearUsuario
+);
+
+// DEFINICIÓN DE RUTA PARA ACTIVAR USUARIO
+router.post(
+  "/activarUsuario",
+  [
+    // VALIDACIONES PARA LOS DATOS DE INICIO DE SESIÓN
+    check("correo", "El correo es obligatorio").isEmail(),
+    // MIDDLEWARE PARA VALIDAR CAMPOS
+    validarCampos,
+  ],
+  usuarioActivar
+);
+
+router.get(
+  "/cambiarContrasenia/:correo",
+  [
+    check("correo", "El id es obligatorio").not().isEmpty(),
+    check("password", "La contrasenia es obligatoria").not().isEmpty(),
+    check("passwordConfirm", "La confirmacion de la contrasenia es obligatoria")
+      .not()
+      .isEmpty(),
+    validarCampos,
+  ],
+  cambiarContrasenia
+);
 
 // EXPORTACIÓN DEL ENRUTADOR
 module.exports = router;
