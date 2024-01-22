d9178d732da09989a81dcb286a904673dbeca79a
CarlosRuiz2023 <juancarlosruizgomez2000@gmail.com>
2024-01-22 14:57:31 -0600

    cambios en la tabla de ausencias añadiendo el campo de tipo_permiso y estatus

--- src/models/modelos/catalogos/ausencias.js
+++ src/models/modelos/catalogos/ausencias.js
@@ -2,6 +2,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const pool = require("../../../database/config");
 
 const Empleado = require("../catalogos/empleado");
+const Permisos = require("./permisos");
 
 const Ausencia = pool.define(
   "cat_ausencias",
@@ -24,6 +25,17 @@ const Ausencia = pool.define(
         key: "id_cat_empleado",
       },
     },
+    fk_cat_permiso: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Permisos,
+        key: "id_cat_permiso",
+      },
+    },
+    estatus: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
   },
   {
     timestamps: false,
@@ -31,5 +43,6 @@ const Ausencia = pool.define(
 );
 
 Ausencia.belongsTo(Empleado, { foreignKey: "fk_cat_empleado", as: "empleado" });
+Ausencia.belongsTo(Permisos, { foreignKey: "fk_cat_permiso", as: "permiso" });
 
 module.exports = Ausencia;
